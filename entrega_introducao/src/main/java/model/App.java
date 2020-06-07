5a9e217b0ab03189bea212b46b4e898da15c02d5
Rodrigo Querino <rodrigoqfcosta@gmail.com>
2020-06-07 13:35:37 -0300

    Inclusao do metodo de teste

--- entrega_introducao/src/main/java/model/App.java
+++ entrega_introducao/src/main/java/model/App.java
@@ -4,15 +4,11 @@ package model;
 import org.apache.commons.math3.util.CombinatoricsUtils;
 
 public class App{
-	public static void main( String[] args ) {
 	
+	public static void main(String[] args) {
 		long factorial = CombinatoricsUtils.factorial(10);
 		System.out.print("Fatorial de 10 = ");
 		System.out.println(factorial);
     }
-
-	public int somar(int x, int y){
-		return (x + y);
-	}
-
+	
 }
