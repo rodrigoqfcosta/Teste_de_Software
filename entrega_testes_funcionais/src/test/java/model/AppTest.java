9c6030d99f2faee2381ffc321cfdf7f964606b6f
Rodrigo Querino <rodrigoqfcosta@gmail.com>
2020-07-13 02:17:14 -0300

    otimizacao testes AppTest

--- entrega_testes_funcionais/src/test/java/model/AppTest.java
+++ entrega_testes_funcionais/src/test/java/model/AppTest.java
@@ -14,56 +14,32 @@ public class AppTest {
     // Casos de testes dos valores validos:
     @Test
     public void CT01RQFC() throws URISyntaxException {
-        JSONObject my_json = new JSONObject();
-        my_json.put("valor_nominal", valorNominal);
-        my_json.put("dia", 1);
-        String str_json = my_json.toString();
-        assertEquals(540, aplicacao.HTTP(str_json));
+        assertEquals(540, aplicacao.HTTP(valorNominal, 1));
     }
 
     @Test
     public void CT02RQFC() throws URISyntaxException {
-        JSONObject my_json = new JSONObject();
-        my_json.put("valor_nominal", valorNominal);
-        my_json.put("dia", 5);
-        String str_json = my_json.toString();
-        assertEquals(540, aplicacao.HTTP(str_json));
+        assertEquals(540, aplicacao.HTTP(valorNominal, 5));
     }
 
     @Test
     public void CT03RQFC() throws URISyntaxException {
-        JSONObject my_json = new JSONObject();
-        my_json.put("valor_nominal", valorNominal);
-        my_json.put("dia", 6);
-        String str_json = my_json.toString();
-        assertEquals(570, aplicacao.HTTP(str_json));
+        assertEquals(570, aplicacao.HTTP(valorNominal, 6));
     }
 
     @Test
     public void CT04RQFC() throws URISyntaxException {
-        JSONObject my_json = new JSONObject();
-        my_json.put("valor_nominal", valorNominal);
-        my_json.put("dia", 10);
-        String str_json = my_json.toString();
-        assertEquals(570, aplicacao.HTTP(str_json));
+        assertEquals(570, aplicacao.HTTP(valorNominal, 10));
     }
 
     @Test
     public void CT05RQFC() throws URISyntaxException {
-        JSONObject my_json = new JSONObject();
-        my_json.put("valor_nominal", valorNominal);
-        my_json.put("dia", 11);
-        String str_json = my_json.toString();
-        assertEquals(600, aplicacao.HTTP(str_json));
+        assertEquals(600, aplicacao.HTTP(valorNominal, 11));
     }
 
     @Test
     public void CT06RQFC() throws URISyntaxException {
-        JSONObject my_json = new JSONObject();
-        my_json.put("valor_nominal", valorNominal);
-        my_json.put("dia", 15);
-        String str_json = my_json.toString();
-        assertEquals(600, aplicacao.HTTP(str_json));
+        assertEquals(600, aplicacao.HTTP(valorNominal, 15));
     }
 
     @Test
@@ -71,17 +47,12 @@ public class AppTest {
         double valor = valorNominal;
         double multa = valor * 0.02;
         double adicional = valor * 0.001;
-
-        JSONObject my_json = new JSONObject();
-        my_json.put("valor_nominal", valorNominal);
-        my_json.put("dia", 16);
-        String str_json = my_json.toString();
-        int dia = (int) my_json.get("dia");
+        int dia = 16;
         if (dia >= 16 && dia < 31) {
             int multiplicador = dia - 16 + 1;
             adicional = valor * (0.001 * multiplicador);
         }
-        assertEquals(valorNominal + adicional + multa, aplicacao.HTTP(str_json));
+        assertEquals(valorNominal + adicional + multa, aplicacao.HTTP(valorNominal, 16));
     }
 
     @Test
@@ -89,35 +60,22 @@ public class AppTest {
         double valor = valorNominal;
         double multa = valor * 0.02;
         double adicional = valor * 0.001;
-
-        JSONObject my_json = new JSONObject();
-        my_json.put("valor_nominal", valorNominal);
-        my_json.put("dia", 30);
-        String str_json = my_json.toString();
-        int dia = (int) my_json.get("dia");
+        int dia = 30;
         if (dia >= 16 && dia < 31) {
             int multiplicador = dia - 16 + 1;
             adicional = valor * (0.001 * multiplicador);
         }
-        assertEquals(valorNominal + adicional + multa, aplicacao.HTTP(str_json));
+        assertEquals(valorNominal + adicional + multa, aplicacao.HTTP(valorNominal, 30));
     }
 
     // Casos de testes dos valores invalidos:
     @Test
     public void CT09RQFC() throws URISyntaxException {
-        JSONObject my_json = new JSONObject();
-        my_json.put("valor_nominal", valorNominal);
-        my_json.put("dia", 31);
-        String str_json = my_json.toString();
-        assertEquals(-1, aplicacao.HTTP(str_json));
+        assertEquals(-1, aplicacao.HTTP(valorNominal, 31));
     }
 
     @Test
     public void CT00RQFC() throws URISyntaxException {
-        JSONObject my_json = new JSONObject();
-        my_json.put("valor_nominal", valorNominal);
-        my_json.put("dia", 0);
-        String str_json = my_json.toString();
-        assertEquals(-1, aplicacao.HTTP(str_json));
+        assertEquals(-1, aplicacao.HTTP(valorNominal, 0));
     }
 }
